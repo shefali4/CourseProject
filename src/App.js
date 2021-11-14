c6d00bdd77587f2037854a4d694777483e1ecbab
Shefali Sharma <shefali4@illinois.edu>
2021-11-13 22:50:04 -0600

    Backend: added logic to add url to list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
+/*globals chrome*/
 
 function App() {
-
   const [url, setUrl] = useState();
+  const urlList = [
+    {
+      url: 'google.com'
+    },
+    {
+      url: 'google.com'
+    },
+    {
+      url: 'google.com'
+    }
+  ];
 
   useEffect(() => {
     const queryInfo = {active: true, lastFocusedWindow: true}
-
     chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
       const url = tabs[0].url;
       setUrl(url);
     })
   })
 
+  const addURL = () => {
+    urlList.push({url});
+    console.log(urlList);
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <h4>Page Mark</h4>
+        <div class="header">
+          <h4>Page Mark</h4>
+          <button class="save-button" onClick={addURL}>+</button>
+        </div>
       </header>
-      <body>
-        <p> URL: </p>
-        <p> {url} </p>
-      </body>
+      <div>
+        {urlList.map(person => (<p>{person.url}</p>))}
+      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
