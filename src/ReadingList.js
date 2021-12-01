c677937a98f30c2af23c14b8221119147fb149ec
Shefali Sharma <shefali4@illinois.edu>
2021-12-01 00:11:52 -0600

    Link storage functionaly works

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -4,66 +4,79 @@ import './ReadingList.scss'
 /*globals chrome*/
 
 export default function Site() {
-    
-    const [site, setSite] = useState();
-    const [urlList, seturlList] = useState([]);
+  
+    let myLeads = []
+    const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+    // Gets previously stored links and renders it
+    window.addEventListener("load", function (){
+        if (leadsFromLocalStorage) {
+            myLeads = leadsFromLocalStorage
+            render(myLeads)
+        }
+    })
+
+
+    // TO DO: allow user to delete individual links
+    let deleteBtn = document.querySelectorAll(".btn-close")
+
+    //TO DO: find document summary/most frequent words, description, extracting important content from pages bookmarked
+
+
+
 
     useEffect(() => {
         const queryInfo = {active: true, lastFocusedWindow: true};
 
-        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-            const url = tabs[0].url;
-            console.log(url)
-            const title = tabs[0].title;
-            const fav="http://www.google.com/s2/favicons?domain="+url;
-            if (url) {
-                const newItem = {
-                    url: url,
-                    title: title,
-                    fav: fav
-                }
-                setSite(newItem);
-                console.log(newItem.fav);
+    // Adds URL to list
+    function addURL(){    
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            const newItem = {
+                url: tabs[0].url,
+                title: tabs[0].title,
+                fav: "http://www.google.com/s2/favicons?domain="+tabs[0].url,
             }
-        });
-    }, []);
+            myLeads.push(newItem)
+            localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+            render(myLeads)
+        })
+    }
 
-    function addURL(e) {
-
-        if ( urlList.filter(function(v) {return v.url === site.url}) ) {
-            seturlList([...urlList, site]);
-            // chrome.storage.local.set(urlList)
-        } 
+    // Deletes all URLs in list
+    function deleteAllURL() {
+        localStorage.clear()
+        myLeads = []
+        render(myLeads)
+    }
         
-        e.preventDefault();
+    // Render function
+    function render(leads) {
+        let listItems = ""
+        for (let i = 0; i < leads.length; i++) {
+            listItems += `
+            <div className="ent">
+                <a className="hyperlink" target='_blank' href='${leads[i].url}'>
+                    <div className="entry">
+                        <div className="prof">
+                            <b className="title"> ${leads[i].title} </b>
+                        </div>
+                        <p> ${leads[i].url} </p>
+                    </div>
+                </a>
+                <button type="button" class="btn-close float-end" aria-label="Close">X</button>
+            </div>
+            `
+        }
+        document.getElementById("ul-el").innerHTML = listItems   
     }
-
-    function del(e) {
-
-        urlList.splice(urlList.findIndex(x => x.id === e.target.value), 1);
-
-        e.preventDefault()
-    }
-
+    
     return (
         <div>
-            <form onSubmit={addURL}>
-                <button type="submit" className="save-button">+</button>
-            </form>
-            <div className="ent">
-                {urlList && urlList.map((item, id) => (
-                    <a className="hyperlink" href={item.url} target="_blank" >
-                        <div onClick={item.url} className="entry" key={id}>
-                            <div className="prof">
-                                <img className="favicon-img" src={item.fav}/>
-                                <b className="title"> {item.title} </b>
-                                {/* <from onSubmit={del(e)} value={item.url}><button type="submit" className="save-button">-</button> </from> */}
-                            </div>
-                            <p> {item.url} </p>
-                        </div>
-                    </a>
-                ))}
-            </div>
+            <button onClick={addURL} className="save-button" type="submit" >+</button>
+            <button onClick={deleteAllURL} id="delete-btn" type="button" >DELETE ALL</button>
+            <ul id="ul-el" class="list-group"></ul>
         </div>
-    );  
-};
+    );
+    
+    }
+)};
