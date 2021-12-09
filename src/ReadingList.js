da616c95562f2209650b518f096a75e91a739cb4
Ashwini Sarvepalli <ashwini1001@gmail.com>
2021-12-09 16:04:13 -0600

    stopwords and cleaned up code

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -10,6 +10,9 @@ export default function Site() {
     const [myLeads, setMyLeads] = useState([])
     const [frag, setFrag] = useState([])
 
+    const stopwords = ". , ? ! ' \" \'' ` `` * - / + a about above according across after afterwards again against albeit all almost alone along already also although always am among amongst an and another any anybody anyhow anyone anything anyway anywhere apart are around as at av be became because become becomes becoming been before beforehand behind being below beside besides between beyond both but by can cannot canst certain cf choose contrariwise cos could cu day do does doesn't doing dost doth double down dual during each either else elsewhere enough et etc even ever every everybody everyone everything everywhere except excepted excepting exception exclude excluding exclusive far farther farthest few ff first for formerly forth forward from front further furthermore furthest get go had halves hardly has hast hath have he hence henceforth her here hereabouts hereafter hereby herein hereto hereupon hers herself him himself hindmost his hither hitherto how however howsoever i ie if in inasmuch inc include included including indeed indoors inside insomuch instead into inward inwards is it its itself just kind kg km last latter latterly less lest let like little ltd many may maybe me meantime meanwhile might moreover most mostly more mr mrs ms much must my myself namely need neither never nevertheless next no nobody none nonetheless noone nope nor not nothing notwithstanding now nowadays nowhere of off often ok on once one only onto or other others otherwise ought our ours ourselves out outside over own per perhaps plenty provide quite rather really round said sake same sang save saw see seeing seem seemed seeming seems seen seldom selves sent several shalt she should shown sideways since slept slew slung slunk smote so some somebody somehow someone something sometime sometimes somewhat somewhere spake spat spoke spoken sprang sprung stave staves still such supposing than that the thee their them themselves then thence thenceforth there thereabout thereabouts thereafter thereby therefore therein thereof thereon thereto thereupon these they this those thou though thrice through throughout thru thus thy thyself till to together too toward towards ugh unable under underneath unless unlike until up upon upward upwards us use used using very via vs want was we week well were what whatever whatsoever when whence whenever whensoever where whereabouts whereafter whereas whereat whereby wherefore wherefrom wherein whereinto whereof whereon wheresoever whereto whereunto whereupon wherever wherewith whether whew which whichever whichsoever while whilst whither who whoa whoever whole whom whomever whomsoever whose whosoever why will wilt with within without worse worst would wow ye yet year yippee you your yours yourself yourselves 1 2 3 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    const stop_array = stopwords.split (" ");
+
     window.addEventListener("load", function () {
         setMyLeads(JSON.parse(localStorage.getItem('myLeads')) || [])
     })
@@ -25,10 +28,7 @@ export default function Site() {
         function (results) {
             // results.length must be 1
             var result = results[0];
-    
-            console.log("most repeated word is:"); // Result: "do"
             var new_result = findMostRepeatedWord(result);
-            console.log(new_result); // Result: "do"
     
             function findMostRepeatedWord(result) {
                 let words = result.match(/\w+/g);
@@ -47,34 +47,25 @@ export default function Site() {
                 let mostRepeatedWord = '';
     
                 for (let word of words) {
-                    // if (occurances[word] > max && !stop_array.includes(word) && word.length >= 2) {
-                    if (occurances[word] > max && word.length >= 2) {
+                    if (occurances[word] > max && !stop_array.includes(word) && word.length >= 2) {
                         max = occurances[word];
                         mostRepeatedWord = word;
                     }
                 }
-    
                 return mostRepeatedWord;
             }
     
             // new result printing on the extension
-            var htmlString = '<head><title>main topic: ' + new_result + '</title></head><body><p>Some text, in a paragraph!</p></body>',
+            var htmlString = '<head><title>main word/topic: ' + new_result + '</title></head>',
                 html = document.createElement('html'),
                 f = document.createDocumentFragment();
-    
     
             html.innerHTML = htmlString;
             f.appendChild(html);
             setFrag(f)
-            // frag = f
         });
 
     }, [myLeads]);
-
-    // var frag;
-
-    // var stopwords = ". , ? ! ' \" \'' ` `` * - / + a about above according across after afterwards again against albeit all almost alone along already also although always am among amongst an and another any anybody anyhow anyone anything anyway anywhere apart are around as at av be became because become becomes becoming been before beforehand behind being below beside besides between beyond both but by can cannot canst certain cf choose contrariwise cos could cu day do does doesn't doing dost doth double down dual during each either else elsewhere enough et etc even ever every everybody everyone everything everywhere except excepted excepting exception exclude excluding exclusive far farther farthest few ff first for formerly forth forward from front further furthermore furthest get go had halves hardly has hast hath have he hence henceforth her here hereabouts hereafter hereby herein hereto hereupon hers herself him himself hindmost his hither hitherto how however howsoever i ie if in inasmuch inc include included including indeed indoors inside insomuch instead into inward inwards is it its itself just kind kg km last latter latterly less lest let like little ltd many may maybe me meantime meanwhile might moreover most mostly more mr mrs ms much must my myself namely need neither never nevertheless next no nobody none nonetheless noone nope nor not nothing notwithstanding now nowadays nowhere of off often ok on once one only onto or other others otherwise ought our ours ourselves out outside over own per perhaps plenty provide quite rather really round said sake same sang save saw see seeing seem seemed seeming seems seen seldom selves sent several shalt she should shown sideways since slept slew slung slunk smote so some somebody somehow someone something sometime sometimes somewhat somewhere spake spat spoke spoken sprang sprung stave staves still such supposing than that the thee their them themselves then thence thenceforth there thereabout thereabouts thereafter thereby therefore therein thereof thereon thereto thereupon these they this those thou though thrice through throughout thru thus thy thyself till to together too toward towards ugh unable under underneath unless unlike until up upon upward upwards us use used using very via vs want was we week well were what whatever whatsoever when whence whenever whensoever where whereabouts whereafter whereas whereat whereby wherefore wherefrom wherein whereinto whereof whereon wheresoever whereto whereunto whereupon wherever wherewith whether whew which whichever whichsoever while whilst whither who whoa whoever whole whom whomever whomsoever whose whosoever why will wilt with within without worse worst would wow ye yet year yippee you your yours yourself yourselves 0 1 2 3 4 5 6 7 8 9 "
-    // var stop_array = stopwords.split (" ");
     
     // Adds URL to list
     function addURL(e) {
@@ -104,9 +95,7 @@ export default function Site() {
     function deleteSingle(e) {
         setMyLeads(myLeads.filter(function (v) {
             return v.url !== e.target.value
-        })
-        )
-
+        }))
     }
 
     function shortenURL(url) {
