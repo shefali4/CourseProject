e9dc0243545eb7078f4a474e0d98ec53a755c1b9
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 <jerryzhang4256@outllook.com>
2021-12-11 14:02:14 -0600

    user text

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -1,3 +1,4 @@
+import { Tune } from '@material-ui/icons'
 import React from 'react'
 import { useState, useEffect } from 'react'
 // import { text } from './stopwords.js';
@@ -11,7 +12,7 @@ export default function Site() {
     const [frag, setFrag] = useState([])
 
     const stopwords = ". , ? ! ' \" \'' ` `` * - / + a about above according across after afterwards again against albeit all almost alone along already also although always am among amongst an and another any anybody anyhow anyone anything anyway anywhere apart are around as at av be became because become becomes becoming been before beforehand behind being below beside besides between beyond both but by can cannot canst certain cf choose contrariwise cos could cu day do does doesn't doing dost doth double down dual during each either else elsewhere enough et etc even ever every everybody everyone everything everywhere except excepted excepting exception exclude excluding exclusive far farther farthest few ff first for formerly forth forward from front further furthermore furthest get go had halves hardly has hast hath have he hence henceforth her here hereabouts hereafter hereby herein hereto hereupon hers herself him himself hindmost his hither hitherto how however howsoever i ie if in inasmuch inc include included including indeed indoors inside insomuch instead into inward inwards is it its itself just kind kg km last latter latterly less lest let like little ltd many may maybe me meantime meanwhile might moreover most mostly more mr mrs ms much must my myself namely need neither never nevertheless next no nobody none nonetheless noone nope nor not nothing notwithstanding now nowadays nowhere of off often ok on once one only onto or other others otherwise ought our ours ourselves out outside over own per perhaps plenty provide quite rather really round said sake same sang save saw see seeing seem seemed seeming seems seen seldom selves sent several shalt she should shown sideways since slept slew slung slunk smote so some somebody somehow someone something sometime sometimes somewhat somewhere spake spat spoke spoken sprang sprung stave staves still such supposing than that the thee their them themselves then thence thenceforth there thereabout thereabouts thereafter thereby therefore therein thereof thereon thereto thereupon these they this those thou though thrice through throughout thru thus thy thyself till to together too toward towards ugh unable under underneath unless unlike until up upon upward upwards us use used using very via vs want was we week well were what whatever whatsoever when whence whenever whensoever where whereabouts whereafter whereas whereat whereby wherefore wherefrom wherein whereinto whereof whereon wheresoever whereto whereunto whereupon wherever wherewith whether whew which whichever whichsoever while whilst whither who whoa whoever whole whom whomever whomsoever whose whosoever why will wilt with within without worse worst would wow ye yet year yippee you your yours yourself yourselves 1 2 3 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z"
-    const stop_array = stopwords.split (" ");
+    const stop_array = stopwords.split(" ");
 
     window.addEventListener("load", function () {
         setMyLeads(JSON.parse(localStorage.getItem('myLeads')) || [])
@@ -25,51 +26,50 @@ export default function Site() {
             allFrames: false, // this is the default
             runAt: 'document_start', // default is document_idle
         },
-        function (results) {
-            // results.length must be 1
-            var result = results[0];
-            var new_result = findMostRepeatedWord(result);
-    
-            function findMostRepeatedWord(result) {
-                let words = result.match(/\w+/g);
-    
-                let occurances = {};
-    
-                for (let word of words) {
-                    if (occurances[word]) {
-                        occurances[word]++;
-                    } else {
-                        occurances[word] = 1;
+            function (results) {
+                // results.length must be 1
+                var result = results[0];
+                var new_result = findMostRepeatedWord(result);
+
+                function findMostRepeatedWord(result) {
+                    let words = result.match(/\w+/g);
+
+                    let occurances = {};
+
+                    for (let word of words) {
+                        if (occurances[word]) {
+                            occurances[word]++;
+                        } else {
+                            occurances[word] = 1;
+                        }
                     }
-                }
-    
-                let max = 0;
-                let mostRepeatedWord = '';
-    
-                for (let word of words) {
-                    if (occurances[word] > max && !stop_array.includes(word) && word.length >= 2) {
-                        max = occurances[word];
-                        mostRepeatedWord = word;
+
+                    let max = 0;
+                    let mostRepeatedWord = '';
+
+                    for (let word of words) {
+                        if (occurances[word] > max && !stop_array.includes(word) && word.length >= 2) {
+                            max = occurances[word];
+                            mostRepeatedWord = word;
+                        }
                     }
+                    return mostRepeatedWord;
                 }
-                return mostRepeatedWord;
-            }
-    
-            // new result printing on the extension
-            var htmlString = '<head><title>main word/topic: ' + new_result + '</title></head>',
-                html = document.createElement('html'),
-                f = document.createDocumentFragment();
-    
-            html.innerHTML = htmlString;
-            f.appendChild(html);
-            setFrag(f)
-        });
+
+                // new result printing on the extension
+                var htmlString = '<head><title>main word/topic: ' + new_result + '</title></head>',
+                    html = document.createElement('html'),
+                    f = document.createDocumentFragment();
+
+                html.innerHTML = htmlString;
+                f.appendChild(html);
+                setFrag(f)
+            });
 
     }, [myLeads]);
-    
+
     // Adds URL to list
     function addURL(e) {
-
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             const newItem = {
                 titleText2: frag.firstChild.getElementsByTagName('title')[0].textContent || frag.firstChild.getElementsByTagName('title')[0].innerText,
@@ -82,8 +82,18 @@ export default function Site() {
             if (!(myLeads.map(a => a.url)).includes(newItem.url)) {
                 setMyLeads([...myLeads, newItem])
             }
+            var input = document.getElementById('input');
+            input.addEventListener("keyup", function (event) {
+                event.preventDefault();
+                if (event.keyCode === 13) {
+                    var usert = document.getElementById("input").value
+                    newItem.titleText2 += ", " + usert
+                    setMyLeads([...myLeads, newItem])
+                    var curt = document.getElementById("input")
+                    curt.remove();
+                }
+            })
         })
-        e.preventDefault();
     }
 
     // Deletes all URLs in list
@@ -108,23 +118,31 @@ export default function Site() {
             <button onClick={deleteAllURL} id="delete-btn" type="button" >DELETE ALL</button>
             <div className="ent">
                 {myLeads && myLeads.map((item, id) => (
-                    <div className="withb">
-                        <a className="hyperlink" target='_blank' href={item.url}>
-                            <div onClick={item.url} className="entry" key={id}>
-                                <div className="prof">
-                                    <img className="favicon-img" src={item.fav} />
-                                    <div className="ti">
-                                        <b className="title"> {item.title} </b>
-                                        <p> {item.shorturl} </p>
+                    <div className="witht" >
+                        <div className="withb" id='curtab'>
+                            <a className="hyperlink" target='_blank' href={item.url}>
+                                <div onClick={item.url} className="entry" key={id}>
+                                    <div className="prof">
+                                        <img className="favicon-img" src={item.fav} />
+                                        <div className="ti">
+                                            <b className="title"> {item.title} </b>
+                                            <p> {item.shorturl} </p>
+                                        </div>
                                     </div>
+                                    <a className="titleText"> {item.titleText2} </a>
                                 </div>
-                                <a className="titleText"> {item.titleText2} </a>
-                            </div>
-                        </a>
-                        <button value={item.url} onClick={deleteSingle} type="button" className="but" aria-label="Close">X</button>
+                            </a>
+                            <button value={item.url} onClick={deleteSingle} type="button" className="but" aria-label="Close">X</button>
+                        </div>
+                        <div className="textbox">
+                            <input type="text" name="name" placeholder='Your topic (enter to save)' id='input' />
+                        </div>
                     </div>
                 ))}
+
+
             </div>
-        </div>
+
+        </div >
     );
 }
